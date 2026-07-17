fb115b96dca8238d247c3b0ba48e3435def7cd2b
karthik200509 <karthikladdu800@gmail.com>
2026-07-17 11:40:15 +0530

    Time: 1 ms (44.15%), Space: 55.2 MB (74.76%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var gcdOfOddEvenSums = function(n) {
-    return n
+   sum1=0;
+   sum2=0
+   let a=n;
+   i=1;
+   while(a!=0){
+    sum1+=i
+    i+=2
+    a--
+   }
+   let b=n;
+   let j=2;
+   while(b!=0){
+     sum2+=j
+     j+=2
+     b--
+   }
+   return Math.abs(sum1-sum2)
 };
