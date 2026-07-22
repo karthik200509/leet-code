48844f52f3f7f5d802e7cafaa753483497c53dfd
karthik200509 <karthikladdu800@gmail.com>
2026-07-22 14:54:16 +0530

    Time: 9 ms (38.19%), Space: 63 MB (21.73%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,14 +3,24 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let res=[];
-    for(let i=0;i<nums.length;i++){
-        res.push(nums[i]*nums[i])
+ let result =[];
+ let left = 0;
+ let rigth = nums.length-1;
+ let index=nums.length-1;
 
+ while(left <= rigth){
+    let leftsquare = nums[left]*nums[left];
+    let rigthsquare = nums[rigth]*nums[rigth]
+
+    if(leftsquare >rigthsquare){
+        result[index]=leftsquare;
+        left++;
+    }else{
+        result[index]=rigthsquare;
+        rigth--;
         
-
     }
-    res.sort((a,b)=>(a-b))
-    return res;
-    
+    index--;
+ }
+ return result;
 };
