5e2432b2865f5fa2e3c4f37961e6d4ff7f3f526d
karthik200509 <karthikladdu800@gmail.com>
2026-07-24 12:58:20 +0530

    Time: 4 ms (90.22%), Space: 63.2 MB (81.67%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(num) {
+    let original = num;
+    let reverse = 0;
+
+    while (num> 0) {
+        let digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+
+    return original === reverse;
+    
+};
