8cb98ec88fc9ecdeef75d9457970d465a002125e
karthik200509 <karthikladdu800@gmail.com>
2026-07-25 15:04:42 +0530

    Time: 0 ms (100%), Space: 53.2 MB (80.8%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,15 +3,25 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let count=0
-  let i =s.length-1;
-  while (i >= 0 && s[i] === ' ') {
-    i--;
-}
- while (i >= 0 && s[i] !== ' ') {
-    count++;
-    i--;
-}
-return count
+    let count = 0;
+    let i = s.length-1;
+    while(i>=0 && s[i]===' '){
+        i--;
+    }
+    while(i>=0 && s[i]!==' '){
+        count++;
+        i--;
+    }
+return count;
+//     let count = 0;
+//   let i =s.length-1;
+//   while (i >= 0 && s[i] === ' ') {
+//     i--;
+// }
+//  while (i >= 0 && s[i] !== ' ') {
+//     count++;
+//     i--;
+// }
+// return count
 
 };
