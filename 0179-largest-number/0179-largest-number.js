415977d5b271497616d00e5888505989bfea595f
karthik200509 <karthikladdu800@gmail.com>
2026-07-26 15:10:56 +0530

    Time: 1 ms (90.29%), Space: 56.8 MB (37.8%) - LeetHub

--- 0179-largest-number/0179-largest-number.js
+++ 0179-largest-number/0179-largest-number.js
@@ -5,7 +5,9 @@
 var largestNumber = function(nums) {
      nums.sort((a, b) => ('' + b + a) - ('' + a + b));
 
-    if (nums[0] === 0) return "0";
+    if (nums[0] === 0){
+        return "0";
+     } 
 
     return nums.join(''); 
 };
