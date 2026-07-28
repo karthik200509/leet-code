a6d6497a37569f7c8336a9746337414776c7bef1
karthik200509 <karthikladdu800@gmail.com>
2026-07-28 12:36:01 +0530

    Time: 0 ms (100%), Space: 53.8 MB (62.6%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-   let row = matrix.length;
-   let colm = matrix[0].length;
+  let row = matrix.length;
+  let colm = matrix[0].length;
 
-   for(let i=0;i<row;i++){
+  for(let i=0;i<row;i++){
     for(let j=0;j<colm;j++){
         if(matrix[i][j]===target){
-            return true;
+            return true
         }
     }
-   }
-   return false;
+  }
+  return false;
 };
