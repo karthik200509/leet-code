bc672c44880b3a6bcc2c5165850b282c2cf3011e
karthik200509 <karthikladdu800@gmail.com>
2026-08-03 19:39:10 +0530

    Time: 8 ms (44.18%), Space: 61.4 MB (34.14%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right) ||
+                   isPalindrome(s, left, right - 1);
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+function isPalindrome(s, low, high) {
+    while (low < high) {
+        if (s[low] !== s[high]) {
+            return false;
+        }
+
+        low++;
+        high--;
+    }
+
+    return true;
+};    
+ 
+
+   
