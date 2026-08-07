739a873b12eb0435a29c96e040ad8dfa48fccb5a
karthik200509 <karthikladdu800@gmail.com>
2026-08-07 15:42:37 +0530

    Time: 0 ms (100%), Space: 54.6 MB (19.31%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,20 +3,39 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    if(num===1){
-        return true;
+    if(num ==- 1){
+       return true;
     }
-  let left=0;
-  let right=num;
-  while(left<=right){
-    let mid=Math.floor((left+right)/2);
-    if(mid*mid===num){
-        return true;
-    }else if(mid*mid<num){
-        left = mid+1
-    }else{
-        right=mid-1
-    }
-  }
-  return false;
+
+      let left = 0;
+      let rigth = num;
+
+      while(left <= rigth){
+        let mid = Math.floor((left+rigth)/2);
+        if(mid*mid === num){
+            return true;
+        }else if(mid*mid < num){
+            left = mid+1
+        }else{
+            rigth = mid-1;
+        }
+      }
+      return false;
 };
+
+//     if(num===1){
+//         return true;
+//     }
+//   let left=0;
+//   let right=num;
+//   while(left<=right){
+//     let mid=Math.floor((left+right)/2);
+//     if(mid*mid===num){
+//         return true;
+//     }else if(mid*mid<num){
+//         left = mid+1
+//     }else{
+//         right=mid-1
+//     }
+//   }
+//   return false;
